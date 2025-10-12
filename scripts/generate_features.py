3abc851a5e3a1bf32252e7c11a20c92f2efae097
Casper Ng <casperngeen@gmail.com>
2025-10-12 16:04:31 -0400

    Add orchestrator and rework function logic

--- scripts/generate_features.py
+++ scripts/generate_features.py
@@ -2,52 +2,87 @@
 import pandas as pd
 import statsmodels.api as sm
 from typing import List
+from enum import Enum
+
+class Feature(Enum):
+    BASIS = "basis"
+    BASIS_Z = "basis_z"
+    BASIS_Z_DIFF = "basis_z_diff"
+    BASIS_Z_ACC = "basis_z_acc"
+    VOLATILITY_RATIO = "vol_ratio"
+    VOLUME_RATIO = "volume_ratio"
+    OFI_RATIO = "ofi_ratio"
+    OFI_Z_RATIO = "ofi_z_ratio"
+    OFI_Z_DIFF_RATIO = "ofi_z_diff_ratio"
+    OFI_Z_ACC_RATIO = "ofi_z_acc_ratio"
+
+'''
+Feature Engineering Orchestration:
+Takes in a list of features to be generated, with relevant window frames 
+'''
+def generate_features(columns: List[Feature], perp: pd.DataFrame, spot: pd.DataFrame, 
+                      basis_window: int, vol_window: int, ofi_window: int, coint_window: int):
+    featureDf = pd.DataFrame()
+    featureDf = generate_basis_features(columns, featureDf, perp, spot, basis_window)
+    featureDf = generate_volatility_features(columns, featureDf, perp, spot, vol_window)
+    featureDf = generate_volume_features(columns, featureDf, perp, spot)
+    featureDf = generate_order_flow_features(columns, featureDf, perp, spot, ofi_window)
+    featureDf = generate_cointegration_features(columns, featureDf, perp, spot, coint_window)
+    return featureDf
+    
 
 '''
 Basis is measure of the spread of perp and spot prices (relative to spot)
 We tune window as a hyperparamater in the model training step
 '''
 def generate_basis_features(columns: List[str], featureDf: pd.DataFrame, perp: pd.DataFrame, spot: pd.DataFrame, window: int):
-    featureDf["basis"] = (perp["close"] - spot["close"]) / spot["close"]
-    mean = featureDf["basis"].rolling(window).mean()
-    std = featureDf["basis"].rolling(window).std(ddof=1)
-    featureDf["basis_z"] = (featureDf["basis"] - mean) / (std + 1e-8)
+    basis = (perp["close"] - spot["close"]) / spot["close"]
+    mean = basis.rolling(window).mean()
+    std = basis.rolling(window).std(ddof=1)
+    basis_z = (basis - mean) / (std + 1e-8)
     
     # we calculate derivatives of basis_z to find how basis is changing
-    featureDf["basis_z_diff"] = featureDf["basis_z"].diff()
-    featureDf["basis_z_acc"] = featureDf["basis_z_diff"].diff()
+    basis_z_diff = basis_z.diff()
+    basis_z_acc = basis_z_diff.diff()
 
-    newColumns = columns + ["basis", "basis_z", "basis_z_diff", "basis_z_acc"]
-    return featureDf, newColumns
+    if Feature.BASIS in columns:
+        featureDf["basis"] = basis
+    if Feature.BASIS_Z in columns:
+        featureDf["basis_z"] = basis_z
+    if Feature.BASIS_Z_DIFF in columns:
+        featureDf["basis_z_diff"] = basis_z_diff
+    if Feature.BASIS_Z_ACC in columns:
+        featureDf["basis_z_acc"] = basis_z_acc
+
+    return featureDf
 
 '''
 Volatility can be used to measure risk
 Different levels of volatility can also indicate periods where different strategies are applied
 '''
 def generate_volatility_features(columns: List[str], featureDf: pd.DataFrame, perp: pd.DataFrame, spot: pd.DataFrame, window: int):
-    perp_change = perp["close"].pct_change()
-    spot_change = spot["close"].pct_change()
-
-    # default value of ddof is 1 -> unbiased estimate of std
-    featureDf["vol"] = (perp_change.rolling(window).std()) / (spot_change.rolling(window).std() + 1e-8)
-    
-    newColumns = columns + ["vol"]
-    return featureDf, newColumns
+    if Feature.VOLATILITY_RATIO in columns:
+        perp_change = perp["close"].pct_change()
+        spot_change = spot["close"].pct_change()
+        
+        # default value of ddof is 1 -> unbiased estimate of std
+        featureDf["vol_ratio"] = (
+            perp_change.rolling(window).std()
+            / (spot_change.rolling(window).std() + 1e-8)
+        )
+    return featureDf
 
 def generate_volume_features(columns: List[str], featureDf: pd.DataFrame, perp: pd.DataFrame, spot: pd.DataFrame):
-    featureDf["volume_ratio"] = perp["volume"] / spot["volume"]
-    newColumns = columns + ["volume_ratio"]
-    return featureDf, newColumns
+    if Feature.VOLUME_RATIO in columns:
+        featureDf["volume_ratio"] = perp["volume"] / spot["volume"]
+    return featureDf
 
 '''
 Order flow imbalance (OFI) tells us about the pressure to sell/buy from either party
 '''
 def generate_order_flow_features(columns: List[str], featureDf: pd.DataFrame, perp: pd.DataFrame, spot: pd.DataFrame, window: int):
     dfs = [perp, spot]
-    ofis = []
-    ofi_zs = []
-    ofi_z_diffs = []
-    ofi_z_accs = []
+    ofis, ofi_zs, ofi_z_diffs, ofi_z_accs = [], [], [], []
     
     for df in dfs:
         buy_vol  = df["taker_buy_base"]
@@ -62,12 +97,15 @@ def generate_order_flow_features(columns: List[str], featureDf: pd.DataFrame, pe
         ofi_z_diffs.append(ofi_z_diff)
         ofi_z_accs.append(ofi_z_acc)
 
-    featureDf["ofi_ratio"] = ofi[0] / ofi[1] 
-    featureDf["ofi_z_ratio"] = ofi_zs[0] / ofi_zs[1]
-    featureDf["ofi_z_diff_ratio"] = ofi_z_diffs[0] / ofi_z_diffs[1]
-    featureDf["ofi_z_acc_ratio"] = ofi_z_accs[0] / ofi_z_accs[1]
-    newColumns = columns + ["ofi", "ofi_z_ratio", "ofi_z_diff_ratio", "ofi_z_acc_ratio"]
-    return featureDf, newColumns
+    if Feature.OFI_RATIO in columns:
+        featureDf["ofi_ratio"] = ofis[0] / (ofis[1] + 1e-8)
+    if Feature.OFI_Z_RATIO in columns:
+        featureDf["ofi_z_ratio"] = ofi_zs[0] / (ofi_zs[1] + 1e-8)
+    if Feature.OFI_Z_DIFF_RATIO in columns:
+        featureDf["ofi_z_diff_ratio"] = ofi_z_diffs[0] / (ofi_z_diffs[1] + 1e-8)
+    if Feature.OFI_Z_ACC_RATIO in columns:
+        featureDf["ofi_z_acc_ratio"] = ofi_z_accs[0] / (ofi_z_accs[1] + 1e-8)
+    return featureDf
 
 '''
 Generate the beta for the hedge ratio between spot and perp
@@ -86,14 +124,20 @@ some fixed relationship in the long run (usually given in the form of hedge rati
 '''
 def generate_cointegration_features(columns: List[str], featureDf: pd.DataFrame, perp: pd.DataFrame, spot: pd.DataFrame, window: int):
     beta = __static_beta(perp, spot)
-    featureDf["spread"] = perp["close"] - beta * spot["close"]
-    
-    mean = featureDf["spread"].rolling(window).mean()
-    std = featureDf["spread"].rolling(window).std()
-    featureDf["spread_z"] = (featureDf["spread"] - mean) / (std + 1e-8)
-    
-    featureDf["spread_z_diff"] = featureDf["spread_z"].diff()
-    featureDf["spread_z_acc"] = featureDf["spread_z_diff"].diff()
+    spread = perp["close"] - beta * spot["close"]
+    mean = spread.rolling(window).mean()
+    std = spread.rolling(window).std(ddof=1)
+    spread_z = (spread - mean) / (std + 1e-8)
+    spread_z_diff = spread_z.diff()
+    spread_z_acc = spread_z_diff.diff()
 
-    newColumns = columns + ["spread", "spread_z", "spread_z_diff", "spread_z_acc"]
-    return featureDf, newColumns
+    if Feature.SPREAD in columns:
+        featureDf["spread"] = spread
+    if Feature.SPREAD_Z in columns:
+        featureDf["spread_z"] = spread_z
+    if Feature.SPREAD_Z_DIFF in columns:
+        featureDf["spread_z_diff"] = spread_z_diff
+    if Feature.SPREAD_Z_ACC in columns:
+        featureDf["spread_z_acc"] = spread_z_acc
+
+    return featureDf
